5accb9650705f3cd2aee394b7cf656447c39c4fb
Carsen Chan <kcchane321@gmail.com>
2018-08-18 01:55:42 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const HtmlWebpackPlugin  = require('html-webpack-plugin')
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+      filename: 'index.bundle.js',
+      path: path.resolve(__dirname, 'public')
+  },
+  context: path.resolve(__dirname, 'src'),
+  plugins: [
+   new HtmlWebpackPlugin({
+          template: 'index.html'
+      })
+  ]
+}
